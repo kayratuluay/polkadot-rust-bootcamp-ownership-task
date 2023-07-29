7470f35ac7ec1a574fb0d5c5917f1b4c71bf0025
Kayra <kayratuluay@gmail.com>
2023-07-29 18:38:22 +0300

    Master 1 | Finished the task

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+
 fn main() {
-    
+    let string1 = String::from("I really");
+    let string2 = String::from(" liked Rust!");
+
+    let concatenated_string = concatenate_strings(&string1, &string2);
+
+    println!("{}",concatenated_string);
 }
 
-fn concatenate_strings(){
-    
+fn concatenate_strings(string_one:&str, string_two:&str) -> String{
+    let mut result = String::from(string_one);
+    result.push_str(string_two);
+    result
 }
